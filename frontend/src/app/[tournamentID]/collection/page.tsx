a00e67a516bde44ff182f9a26d7dc40949230307
Joaquin Leon <leonjoaquin77@gmail.com>
2024-03-12 00:02:29 -0300

    feat: Add filters

--- frontend/src/app/[tournamentID]/collection/page.tsx
+++ frontend/src/app/[tournamentID]/collection/page.tsx
@@ -1,23 +1,16 @@
 "use client"
 
-import { CardDisplay } from "@/components/card";
 import { Header } from "@/components/header";
 import Layout from "@/components/layout";
-import { PackList } from "@/components/packlist";
-import { ApiGetRequest, ApiPostRequest } from "@/requests/requests";
-import { CardData, OwnedCard } from "@/types/card";
-import { BoosterPack, BoosterPackData, TournamentPlayer } from "@/types/tournamentPlayer";
-import { Autocomplete, AutocompleteItem, Button, Input } from "@nextui-org/react";
-import { useRouter } from "next/navigation";
+import { ApiGetRequest } from "@/requests/requests";
+import { OwnedCard } from "@/types/card";
 import { useEffect, useState } from "react";
-import { BsFillTrashFill } from "react-icons/bs";
 import { CardImage } from "@/components/card"
+import { Input, Dropdown, DropdownItem, DropdownMenu, DropdownTrigger, Button } from "@nextui-org/react";
 
 
 
 export default function CollectionPage(props: any) {
-  let router = useRouter()
-  let [tournamentPlayer, setTournamentPlayer] = useState<TournamentPlayer>()
   let [error, setError] = useState<string>("")
   let [currentCards, setCurrentCards] = useState<OwnedCard[]>([])
 
@@ -25,6 +18,7 @@ export default function CollectionPage(props: any) {
     ApiGetRequest({
       route: "/collection",
       query: {
+        filters: "color<R",
         tournamentID: props.params.tournamentID,
         count: 75,
         page: 1,
@@ -41,8 +35,42 @@ export default function CollectionPage(props: any) {
     <Layout tournamentID={props.params.tournamentID}>
       <div className="mx-16 my-16">
         <Header title="Collection" />
+        <div className="flex flex-row gap-2 mb-12 items-center">
+          <Input
+            onChange={(e) => console.log(e)}
+            variant="bordered"
+            type="string"
+            placeholder="Set code"
+            className="text-white"
+          />
+          <Dropdown>
+            <DropdownTrigger>
+              <Button
+                variant="bordered"
+                className="capitalize"
+              >
+                {/* {selectedValue} */}
+                Any rarity
+              </Button>
+            </DropdownTrigger>
+            <DropdownMenu
+              onChange={(e) => console.log(e)}
+              className="text-white"
+
+            >
+              <DropdownItem key="any">Any rarity</DropdownItem>
+              <DropdownItem key="common">Common</DropdownItem>
+              <DropdownItem key="uncommon">Uncommon</DropdownItem>
+              <DropdownItem key="rare">Rare</DropdownItem>
+              <DropdownItem key="mythic">Mythic Rare</DropdownItem>
+            </DropdownMenu>
+          </Dropdown>
+          <Button>
+            <i className="ms ms-u text-mana-blue"></i>
+          </Button>
+        </div>
         <div className="flex flex-row gap-2 justify-between">
-          <CollectionDisplay cards={currentCards} />
+          {currentCards && < CollectionDisplay cards={currentCards} />}
         </div>
       </div>
     </Layout >
@@ -52,8 +80,6 @@ export default function CollectionPage(props: any) {
 export type CollectionDisplayProps = {
   cards: OwnedCard[]
 }
-
-
 
 export function CollectionDisplay(props: CollectionDisplayProps) {
   return (
--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -22,6 +22,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <link href="//cdn.jsdelivr.net/npm/mana-font@latest/css/mana.css" rel="stylesheet" type="text/css" />
       <body className={`dark ${inter.className}`}>
         <NextUIProvider>
           {children}
